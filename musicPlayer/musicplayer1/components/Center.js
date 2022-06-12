38b6fea853c21200a7708057a5de553893c8b9ae
Robert Albenda <ralbenda@gmail.com>
2022-06-12 19:45:39 -0400

    progress

--- musicPlayer/musicplayer1/components/Center.js
+++ musicPlayer/musicplayer1/components/Center.js
@@ -2,8 +2,17 @@ function Center() {
 
 
   return (
-    <div className=" flex flex-grow text-white">
-        <h1>Center Console Screen</h1>
+    <div className="flex-grow text-white ">
+        <header className="absolute top-5 right-8">
+          <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+            <img src='' alt='' />
+            <br />
+            <h2>Welcome to your Splice Page</h2>
+          </div>
+        </header>
+        <section className= {`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white padding-8`}>
+            <h1>hello</h1>
+        </section>
     </div>
   )
 }
