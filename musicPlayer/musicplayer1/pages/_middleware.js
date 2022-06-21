8cffafa3ba68c26cdddd7a711e0aaa31b99f952a
Robert Albenda <ralbenda@gmail.com>
2022-06-21 11:40:49 -0400

    middleware progress

---
+++ musicPlayer/musicplayer1/pages/_middleware.js
@@ -0,0 +1,27 @@
+import { getToken } from "next-auth/jwt"
+import { NextRequest} from "next/server"
+import { NextResponse } from "next/server"
+
+
+export async function middleware(req) {
+
+    //token will exist if user is logged in
+  const token = await getToken({ req, secret: process.env.JWT_SECRET });
+
+  const {  pathname } = req.nextUrl;
+  const url = req.nextUrl.clone()
+  url.pathname = '/login'
+  //allow the request if: 
+  //1) its a request for next-auth session and provider fetching
+  //2) token exists
+
+  if(pathname.includes("/api/auth") || token ) {
+
+    return NextResponse.next();
+  }
+  //redirect them to login page if they dont have token and are requesting protected route
+
+  if(!token && pathname!=="/login") {
+    return NextResponse.rewrite(url)
+  }
+}
