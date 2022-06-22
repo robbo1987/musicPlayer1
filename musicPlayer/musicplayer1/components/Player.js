ee0613c253ce0ecc2eb9fa90f6a9ca267b6658a2
Robert Albenda <ralbenda@gmail.com>
2022-06-22 13:08:31 -0400

    working on volume

--- musicPlayer/musicplayer1/components/Player.js
+++ musicPlayer/musicplayer1/components/Player.js
@@ -1,64 +1,73 @@
-import React, { useEffect } from 'react'
-import { useSession } from 'next-auth/react'
-import { useState } from "react"
-import { useRecoilState} from 'recoil'
-import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
-import useSpotify from '../hooks/useSpotify'
-import useSongInfo  from "../hooks/useSongInfo"
-import {FastForwardIcon, PauseIcon, PlayIcon, ReplyIcon, SwitchHorizontalIcon } from '@heroicons/react/outline'
-import { RewindIcon } from '@heroicons/react/solid'
-
+import React, { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
+import useSpotify from "../hooks/useSpotify";
+import useSongInfo from "../hooks/useSongInfo";
+import {
+  FastForwardIcon,
+  PauseIcon,
+  PlayIcon,
+  ReplyIcon,
+  SwitchHorizontalIcon,
+  VolumeUpIcon,
+} from "@heroicons/react/outline";
+import { RewindIcon } from "@heroicons/react/solid";
 
 const Player = () => {
-  const spotifyApi = useSpotify()
-  const { data: session,status } = useSession()
-  const [currentTrackId, setCurrentIdTrack] = useRecoilState(currentTrackIdState)
-  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
-  const [volume, setVolume] = useState(50)
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+  const [currentTrackId, setCurrentIdTrack] =
+    useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+  const [volume, setVolume] = useState(50);
 
   const handlePlayPause = () => {
-    spotifyApi.getMyCurrentPlaybackState().then(data=> {
-      if(data.body.is_playing) {
+    spotifyApi.getMyCurrentPlaybackState().then((data) => {
+      if (data.body.is_playing) {
         spotifyApi.pause();
-        setIsPlaying(false)
+        setIsPlaying(false);
       } else {
         spotifyApi.play();
-        setIsPlaying(true)
+        setIsPlaying(true);
       }
-    })
+    });
+  };
 
-  }
-
-  const songInfo = useSongInfo()
+  const songInfo = useSongInfo();
   const fetchCurrentSong = () => {
     if (!songInfo) {
-      spotifyApi.getMyCurrentPlayingTrack().then(data => {
-        setCurrentIdTrack(data.body?.item?.id)
+      spotifyApi.getMyCurrentPlayingTrack().then((data) => {
+        setCurrentIdTrack(data.body?.item?.id);
 
-      spotifyApi.getMyCurrentPlaybackState().then(data=> {
-        setIsPlaying(data.body?.is_playing)
-      })
-      })
+        spotifyApi.getMyCurrentPlaybackState().then((data) => {
+          setIsPlaying(data.body?.is_playing);
+        });
+      });
     }
-  }
+  };
 
   useEffect(() => {
-      if(spotifyApi.getAccessToken() && !currentTrackId) {
-          fetchCurrentSong();
-          setVolume(50)
-      }
-
-  }, [currentTrackIdState,spotifyApi, session])
+    if (spotifyApi.getAccessToken() && !currentTrackId) {
+      fetchCurrentSong();
+      setVolume(50);
+    }
+  }, [currentTrackIdState, spotifyApi, session]);
 
   return (
-    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white
-    grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
-    
-      <div className="flex items-center space-x-4" >
-        <img className="hidden md:inline h-10 w-10"
-        src={songInfo?.album.images?.[0]?.url} alt="" />
-     
-        <div> 
+    <div
+      className="h-24 bg-gradient-to-b from-black to-gray-900 text-white
+    grid grid-cols-3 text-xs md:text-base px-2 md:px-8"
+    >
+      <div className="flex items-center space-x-4">
+        <img
+          className="hidden md:inline h-10 w-10"
+          src={songInfo?.album.images?.[0]?.url}
+          alt=""
+        />
+
+        <div>
           <h3> {songInfo?.name}</h3>
           <p> {songInfo?.artists?.[0]?.name}</p>
         </div>
@@ -66,24 +75,43 @@ const Player = () => {
 
       <div className="flex items-center justify-evenly">
         <SwitchHorizontalIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
-        <RewindIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
-        onClick={()=> spotifyApi.skipToPrevious()}  />
+        <RewindIcon
+          className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
+          onClick={() => spotifyApi.skipToPrevious()}
+        />
         {isPlaying ? (
-          <PauseIcon className="button w-10 h-10"
-          onClick={handlePlayPause} />  
+          <PauseIcon className="button w-10 h-10" onClick={handlePlayPause} />
         ) : (
-          <PlayIcon className="button w-10 h-10"
-          onClick={handlePlayPause} />
+          <PlayIcon className="button w-10 h-10" onClick={handlePlayPause} />
         )}
 
-        <FastForwardIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
-        onClick={()=> spotifyApi.skipToNext()}/>
-        <ReplyIcon  className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"/>
+        <FastForwardIcon
+          className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
+          onClick={() => spotifyApi.skipToNext()}
+        />
+        <ReplyIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
       </div>
-    
-    </div>
-    
-  )
-}
 
-export default Player
+      <div className="flex items-center space-x-3 md:space-x-4 justify-end pr-5">
+        <VolumeUpIcon
+          className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
+          onClick={() => volume > 0 && setVolume(volume - 10)}
+        />
+        <input
+          className="w-14 md:w-28"
+          type="range"
+          value={volume}
+          onChange={(event) => setVolume(Number(event.target.value))}
+          min={0}
+          max={100}
+        />
+        <VolumeUpIcon
+          className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
+          onClick={() => volume > 0 && volume < 100 && setVolume(volume + 10)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
