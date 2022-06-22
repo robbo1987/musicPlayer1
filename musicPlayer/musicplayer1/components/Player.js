62e3f4f8b8f9bc309ad955c27274016f5aa79fae
Robert Albenda <ralbenda@gmail.com>
2022-06-22 11:02:42 -0400

    upgrades to player

--- musicPlayer/musicplayer1/components/Player.js
+++ musicPlayer/musicplayer1/components/Player.js
@@ -1,22 +1,88 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSession } from 'next-auth/react'
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { useState } from "react"
+import { useRecoilState} from 'recoil'
 import { currentTrackIdState, isPlayingState } from '../atoms/songAtom'
 import useSpotify from '../hooks/useSpotify'
+import useSongInfo  from "../hooks/useSongInfo"
+import {FastForwardIcon, PauseIcon, PlayIcon, ReplyIcon, SwitchHorizontalIcon } from '@heroicons/react/outline'
+import { RewindIcon } from '@heroicons/react/solid'
 
 
 const Player = () => {
   const spotifyApi = useSpotify()
   const { data: session,status } = useSession()
-  const [currentTrackId, setCurrentIdTrack] = useRecoilState(isPlayingState)
-  const [voume, setVolume] = useState(50)
+  const [currentTrackId, setCurrentIdTrack] = useRecoilState(currentTrackIdState)
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
+  const [volume, setVolume] = useState(50)
+
+  const handlePlayPause = () => {
+    spotifyApi.getMyCurrentPlaybackState().then(data=> {
+      if(data.body.is_playing) {
+        spotifyApi.pause();
+        setIsPlaying(false)
+      } else {
+        spotifyApi.play();
+        setIsPlaying(true)
+      }
+    })
+
+  }
+
+  const songInfo = useSongInfo()
+  const fetchCurrentSong = () => {
+    if (!songInfo) {
+      spotifyApi.getMyCurrentPlayingTrack().then(data => {
+        setCurrentIdTrack(data.body?.item?.id)
+
+      spotifyApi.getMyCurrentPlaybackState().then(data=> {
+        setIsPlaying(data.body?.is_playing)
+      })
+      })
+    }
+  }
+
+  useEffect(() => {
+      if(spotifyApi.getAccessToken() && !currentTrackId) {
+          fetchCurrentSong();
+          setVolume(50)
+      }
+
+  }, [currentTrackIdState,spotifyApi, session])
+
   return (
-    <div>
-      <div>
-        <img src="" alt="" />
+    <div className="h-24 bg-gradient-to-b from-black to-gray-900 text-white
+    grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
+    
+      <div className="flex items-center space-x-4" >
+        <img className="hidden md:inline h-10 w-10"
+        src={songInfo?.album.images?.[0]?.url} alt="" />
+     
+        <div> 
+          <h3> {songInfo?.name}</h3>
+          <p> {songInfo?.artists?.[0]?.name}</p>
+        </div>
       </div>
-      
+
+      <div className="flex items-center justify-evenly">
+        <SwitchHorizontalIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out" />
+        <RewindIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
+        onClick={()=> spotifyApi.skipToPrevious()}  />
+        {isPlaying ? (
+          <PauseIcon className="button w-10 h-10"
+          onClick={handlePlayPause} />  
+        ) : (
+          <PlayIcon className="button w-10 h-10"
+          onClick={handlePlayPause} />
+        )}
+
+        <FastForwardIcon className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"
+        onClick={()=> spotifyApi.skipToNext()}/>
+        <ReplyIcon  className="w-5 h-5 cursor-pointer hover:scale-125 transition transform duration-100 ease-out"/>
+      </div>
+    
     </div>
+    
   )
 }
 
